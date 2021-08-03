3895f50706d7a0c94fbff442f2663bb26e3a7e08
santoshgusain <santosh1998gusain@gmail.com>
2021-08-03 17:23:23 +0530

    change is app file

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const db = require('./config/db');
-// Set static folder
-app.use(express.static("client/dist"));
-
-
-app.get('test',(req,res)=>{
-  res.send('Working fine buddy!');
-});
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/dist"));
+  // For resolving path
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
+
+app.get('/',(req,res)=>{
+  res.send('Welcome to Home Page');
+});
+app.get('test',(req,res)=>{
+  res.send('Working fine buddy!');
+});
+
 
 app.listen(PORT, (err)=>{
     if (err)
